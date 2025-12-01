d57c743206a5db6d66830879cb3b32cc4a4576cb
diegoandresagudelo63-rgb <diegoandresagudelo63@gmail.com>
2025-12-01 10:54:26 -0500

    Commit practica Git Hub

--- mensaje.js
+++ mensaje.js
@@ -1,1 +1,2 @@
 alert("Hola usuario, bienvenido a mi repositorio de practica de GIT y GIT HUB!");
+console.log("Este es un mensaje en la consola para verificar que el archivo mensaje.js se ha cargado correctamente.");
